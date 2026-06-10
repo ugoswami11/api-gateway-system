b89160af314b31e681272debff70ad9e6d6b745b
ugoswami11 <ugoswami1112@gmail.com>
2026-06-10 10:41:31 +0530

    load-balancer-integration-V5.3

--- api-gateway-system/user-service/src/main/java/com/gateway/userservice/controller/UserController.java
+++ api-gateway-system/user-service/src/main/java/com/gateway/userservice/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.gateway.userservice.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,19 +10,26 @@ public class UserController {
 
     private int counter = 0;
 
+    @Value("${server.port}")
+    private String port;
+
     @GetMapping("/users")
     public ResponseEntity<String> users() throws Exception {
 
-//        Thread.sleep(7000);
+////        Thread.sleep(7000);
+//
+//        counter++;
+//
+//        if (counter < 3) {
+//            throw new RuntimeException("Temporary failure");
+//        }
+//
+//        return ResponseEntity.ok("Success after retry");
+//
+////        return ResponseEntity.ok("Users fetched");
 
-        counter++;
+        return ResponseEntity.ok(
+                "Response from USER-SERVICE running on port: " + port);
 
-        if (counter < 3) {
-            throw new RuntimeException("Temporary failure");
-        }
-
-        return ResponseEntity.ok("Success after retry");
-
-//        return ResponseEntity.ok("Users fetched");
     }
 }
